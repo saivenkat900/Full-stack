125cdadb487928f61d997eceffa07f26bd9ac87c
saivenkat900 <99874195+saivenkat900@users.noreply.github.com>
2022-03-16 12:01:58 +0530

    Add files via upload

---
+++ day-20 condition stmt and alert prompt/index.js
@@ -0,0 +1,51 @@
+/// students marks list
+
+// a=25
+// if (a>=25){
+//     console.log("your are eligible for the job")
+// }
+
+
+//   else{
+//     console.log("you are not eligible fot the job")
+//   }
+
+//   a=25
+//   if (a>25 && a==25){
+//       console.log("your are eligible for the job")
+//   }
+  
+  
+//     else{
+//       console.log("you are not eligible fot the job")
+//     }
+  
+/// grades of the student
+/// name= sai
+
+marks=100
+
+if (marks>90){
+    console.log("got the 'A' grade")
+}
+else{
+    console.log("got the 'b' grade")
+ }
+
+ if (marks>80){
+     console.log("got the 'c' grade")
+ }
+ if (marks>70){
+    console.log("got the 'd' grade")
+}
+if (marks>60){
+    console.log("got the 'e' grade")
+}
+if (marks>50){
+    console.log("got the 'f' grade")
+}
+
+alert("give a strong password")
+
+b = prompt("Enter you name")
+  console.log("the name you enterted is :",b) 
