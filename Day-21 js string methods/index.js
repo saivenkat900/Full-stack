94edcbe4828fb73d6aa609af2020228f5750df8e
saivenkat900 <99874195+saivenkat900@users.noreply.github.com>
2022-03-22 00:29:32 +0530

    Add files via upload

--- Day-21 js string methods/index.js
+++ Day-21 js string methods/index.js
@@ -1,5 +1,21 @@
  txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
- console.log(length = txt.length
+ console.log(length = txt.length)
+
+ str = "Apple, Banana, Kiwi"
+ console.log(str.slice(7, 13))
 
  str = "Apple, Banana, Kiwi";
- console.log(str.slice(7, 13))
+ console.log(str.slice(-12, -6))
+
+ console.log(str.slice(7))
+
+ console.log(str.slice(-12))
+
+ str = "Apple, Banana, Kiwi"
+ console.log(str.substring(7, 13))
+
+ str = "Apple, Banana, Kiwi"
+ console.log(str.substr(7, 6))
+
+ str = "Apple, Banana, Kiwi"
+ console.log(str.substr(-4))
