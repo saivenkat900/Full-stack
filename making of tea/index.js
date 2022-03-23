f0083bfd1e2aa88ecdcc1e3ff2f36c18a25b41d3
saivenkat900 <99874195+saivenkat900@users.noreply.github.com>
2022-03-23 12:52:29 +0530

    Add files via upload

---
+++ making of tea/index.js
@@ -0,0 +1,28 @@
+// requiredmaterials=["stove","gas cylinder","gas light","steel bowl"]
+
+// ingrediants=["water","milk","sugar","tea powder"]
+
+// console.log("procedure of making tea")
+
+// function procedure(username){
+//     a  = user.toString()
+//     if(a.includes(username)){
+//         console.log("fix the stove with gas")
+//     }
+//     else{
+//         console.warn("it will not work")
+//     }
+// }
+
+// user='water'
+
+function makingoftea(){
+    console.log("Required materials:stove,gas cylinder,gas lighter,steel vessel,tea strainer")
+    console.log("required items:\n1.milk \n2.water \n3.sugar \n4.tea powder")
+    console.log(" step:1 first fix the stove with gas and later turn on the gas with gas light\n step:2 take a steel vessel and pour some water in to that\n step:3 burn up to 1 minute untill gets heated\n step:4 take a milk and pour in to a burned water and add some sugar on the milk\n step:4 heat up to 2 min and add some tea powder on it until changes the color of the milk\n step:5 finally after changes of the color of tea filter with a tea strainer and later pour in the tea cup\n have some cup of tea with a pleasent smell  ")
+}
+
+a=makingoftea()
+console.log(a)
+
+
